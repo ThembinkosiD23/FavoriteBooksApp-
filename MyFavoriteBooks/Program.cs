24660635a6719e9f8a29bcdd696a879368d8bac4
ThembinkosiD23 <dladlathembinkosi75@gmail.com>
2025-11-08 17:15:48 +0200

    feat: Created the list to store the books

--- MyFavoriteBooks/Program.cs
+++ MyFavoriteBooks/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System.IO;
+using System.Collections.Generic;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Welcome to the FavoriteBooksApp!");
+        Console.WriteLine("Please Enter your 5 Favarite Books, separate them by comma \",\"");
+        string listNames = Console.ReadLine();
+
+        //convet the string to the list
+        List<string> favBooks = listNames.Split(',').Select(listNames => listNames).ToList();
+
+
     }
 }

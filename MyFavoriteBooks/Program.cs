22e66b5c8edebdd100ce77e6918a0d0613ddbbf9
ThembinkosiD23 <dladlathembinkosi75@gmail.com>
2025-11-08 17:51:06 +0200

    feat: Added the feature to display the list of the books

--- MyFavoriteBooks/Program.cs
+++ MyFavoriteBooks/Program.cs
@@ -5,30 +5,39 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the FavoriteBooksApp!");
-        Console.WriteLine("Please Enter your 5 Favarite Books, separate them by comma \",\"");
+        Console.WriteLine("Please enter your 5 favorite books, separated by commas (e.g. Book1, Book2, Book3, Book4, Book5):");
         string listNames = Console.ReadLine();
 
         //convet the string to the list
         List<string> favBooks = listNames.Split(',').Select(listNames => listNames).ToList();
 
         //write the favBooks to the file called books
-        string pathfile = "C:\\Users\\ThembinkosiDladla\\OneDrive - Prosus-Naspers\\Desktop\\LEARN25\\C#\\Fundamentals\\Projects\\MyFavoriteBooks\\Files\\Books.txt";
+        string filePath = "C:\\Users\\ThembinkosiDladla\\OneDrive - Prosus-Naspers\\Desktop\\LEARN25\\C#\\Fundamentals\\Projects\\MyFavoriteBooks\\Files\\Books.txt";
 
         //chek if the file is there
-        if (!File.Exists(pathfile))
+        if (!File.Exists(filePath))
         {
             //create the file and write on it
-            using StreamWriter writer = new StreamWriter(pathfile);
+            using StreamWriter writer = new StreamWriter(filePath);
             //write the list of favBook in the file
             foreach (string favBook in favBooks)
             {
                 writer.WriteLine(favBook);
+                Console.WriteLine("Your Favorite Books has been added to the file!");
             }
 
         }
         else
         {
             Console.WriteLine("The file is already there!");
+        }
+
+        // --- READ AND DISPLAY FILE CONTENT ---
+        Console.WriteLine("Here is the list of your favorite books:");
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string content = reader.ReadToEnd();
+            Console.WriteLine(content);
         }
 
     }
